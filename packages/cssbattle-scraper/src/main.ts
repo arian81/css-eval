5d908b804ee7c750527ed27872df1d51c1d7cf9c
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-12-03 19:40:26 -0500

    feat: move scraped images to data folder

--- packages/cssbattle-scraper/src/main.ts
+++ packages/cssbattle-scraper/src/main.ts
@@ -1,11 +1,11 @@
 import { Dataset } from "crawlee";
-import { writeFileSync } from "node:fs";
+import fs from "fs";
 import { crawler, generateUrls } from "./scraper.js";
 import { challegeSchema, Challenge } from "./types.js";
 
 const urls = generateUrls();
 await crawler.run(urls);
-const dataset = await Dataset.open('challenges');
+const dataset = await Dataset.open("challenges");
 const { items } = await dataset.getData();
 
 const validatedItems = challegeSchema.array().safeParse(items);
@@ -14,13 +14,10 @@ if (!validatedItems.success) {
   process.exit(1);
 }
 // flat object is probably the easiest to process later
-const sortedItems = validatedItems.data.sort((a, b) => {
-  if (a.year !== b.year) return a.year - b.year;
-  if (a.month !== b.month) return a.month - b.month;
-  return a.challengeId.localeCompare(b.challengeId);
-});
-
-writeFileSync(
-  "./storage/challenges.json",
-  JSON.stringify(sortedItems, null, 2),
+const sortedItems = validatedItems.data.sort(
+  (a, b) => new Date(a.name).getTime() - new Date(b.name).getTime(),
 );
+
+fs.writeFileSync("./src/challenges.json", JSON.stringify(sortedItems, null, 2));
+fs.cpSync("./src/challenges.json", "../../src/data/challenges.json", {recursive: true})
+fs.cpSync("./storage/key_value_stores/images", "../../src/data/images", {recursive: true})
