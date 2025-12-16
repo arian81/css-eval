a14612d08c88e84d566910d2a080dcdf3a62ac8d
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-12-16 00:06:45 -0500

    fix: force dynamic page to have random redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,16 @@ import type { ChallengesDictionary } from "@packages/cssbattle-scraper/src/types
 const challenges: ChallengesDictionary = data;
 const challengeIds = Object.keys(challenges);
 
+export const dynamic = 'force-dynamic';
+
+
 export default function Page() {
   // const router = useRouter();
   // Server only component, so no useEffect
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const randomId = challengeIds[Math.floor(Math.random() * challengeIds.length)];
 
-  redirect(`/challenge/${randomId}`);
+  redirect(`/challenge/${randomId}`, );
 
   return <div></div>;
 }
